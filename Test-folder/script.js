f88928f1e5abf1229cc0c421f84257bd73041806
FrancisMurayaDev <francismurayadev@gmail.com>
2025-07-01 20:06:02 +0300

    update notes

--- Test-folder/script.js
+++ Test-folder/script.js
@@ -1,5 +1,7 @@
-const button = document.querySelector(".btn");
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-button.addEventListener("click", function () {
-  alert("I just go clicked!");
+const firstOddNumber = myArray.find(element => {
+  if (element % 2 !== 0) return element;
 });
+
+console.log(firstOddNumber);
