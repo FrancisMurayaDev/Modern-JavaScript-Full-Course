fa2665b6fff3aaaf3326a4fc0d01d0cb36c0ffca
FrancisMurayaDev <francismurayadev@gmail.com>
2025-08-25 09:55:32 +0300

    update notes

---
+++ Test-folder/script.js
@@ -0,0 +1,33 @@
+let age = 30;
+
+if (age >= 18) {
+  console.log(`You are of legal age to drive`);
+} else if (age === 17) {
+  console.log(`You only have one year to wait. Just chill!`);
+} else if (age <= 16) {
+  console.log(`You cannot drive!`);
+}
+
+// SWITCH STATEMENT
+
+let day = `Thursday`;
+
+switch (day) {
+  case `Sunday`:
+    console.log(`Its on Sabath!`);
+    break;
+
+  case "Monday":
+    console.log(`The first day of the week!`);
+    break;
+
+  case `Friday`:
+    console.log(`Here is the weekend!`);
+    break;
+
+  default:
+    console.log(`Its just a normal day of the week!`);
+    break;
+}
+
+let;
